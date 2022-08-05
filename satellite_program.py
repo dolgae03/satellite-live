7c7677659a7ed5bbce5292e07612fc04b8b81b27
unknown <9574m@kaist.ac.kr>
2022-08-05 16:22:50 +0900

    first commit

---
+++ satellite_program.py
@@ -0,0 +1,444 @@
+import socket
+import time
+
+from skyfield.api import Loader, wgs84, EarthSatellite
+
+import pyvista as pv
+import math
+import numpy as np
+import struct
+
+from threading import Thread, Lock
+
+class satrac_info:
+    def __init__(self, t, location, attitude, vector):
+        self.t = t
+        self.location = location
+        self.attitude = attitude
+        self.vector = vector
+        
+        
+class program_manage:
+    def __init__(self, gs_location, sat_location):
+        self.load = Loader('./data')
+        self.gs_location = gs_location
+        self.sat_location = sat_location
+        self.actor = {'satellite_axis':[],'earth_axis':[], 'satellite_vector':{}, 'satellite_dark_trajectory':[], 'satellite_bright_trajectory':[]}
+        self.polydata = {}
+
+        for key in self.actor.keys():
+            self.actor[key]
+
+        self.earth_radius = 6378.1
+
+        self.eph = self.load('de421.bsp')
+
+        self.tle = ['','','']
+        self.lock = Lock()
+        self.ts = self.load.timescale()
+
+        self.trajectory_exist = False
+
+        self.trajectory_active = True
+        self.satellite_active = True
+        self.satellite_vector_active = True
+        self.earth_axis_active = True
+        self.time_gap = None
+
+    def start_program(self):
+        global gs_mesh
+        global recieve_time, recieve_location, recieve_attitude, recieve_vec
+
+        sphere = pv.Sphere(radius=self.earth_radius, theta_resolution=240, phi_resolution=240, start_theta=270.0001, end_theta=270)
+        sphere.active_t_coords = np.zeros((sphere.points.shape[0], 2))
+
+        for i in range(sphere.points.shape[0]):
+            x, y, z = sphere.points[i, 0]/self.earth_radius, sphere.points[i, 1]/self.earth_radius, sphere.points[i, 2]/self.earth_radius
+            x, y, z = self.normalize(x), self.normalize(y), self.normalize(z)
+                
+            sphere.active_t_coords[i] = [0.5 + math.atan2(-x, y)/(2 * math.pi), 0.5 + math.asin(z)/math.pi]
+            
+        sphere.rotate_z(-90)
+
+        self.pl = pv.Plotter()
+        self.actor['backgroud'] = self.pl.add_background_image('./data/starry-night-sky-fit.jpg', scale=1.001)
+        self.actor['earth'] = self.pl.add_mesh(sphere, texture=pv.read_texture("./data/earth.jpg"), smooth_shading=False)
+        
+        self.color = ['red','green','blue','yellow','purple','orange', 'white', 'skyblue']
+        self.sat_dcm = np.array([[1,0,0],[0,1,0],[0,0,1]])
+
+        basis = np.array([[500,0,0],
+                            [0,500,0],
+                            [0,0,500]])
+        sat_att = (self.sat_dcm @ basis).T
+
+        reader = pv.get_reader('./data/antenna.stl')
+        gs_mesh = reader.read()
+        gs_scale = 2
+        gs_dcm = rotation_matrix_from_vectors(np.array([0,1,0]), np.array(gs_location))
+
+        for i in range(gs_mesh.points.shape[0]):
+            gs_mesh.points[i] = (gs_dcm @ (gs_mesh.points[i].T)).T * gs_scale + gs_location
+
+        #for i in range(sat_mesh.points.shape[0]):
+        #    sat_mesh.points[i] = ((sat_dcm @ (sat_mesh.points[i].T)).T)*sat_scale + sat_location
+
+        self.actor['groundstation'] = self.pl.add_mesh(gs_mesh)
+        #self.actor['satellite'] = pl.add_mesh(sat_mesh)
+
+
+        for i in range(3):
+            temp = pv.PolyData(np.array([self.sat_location + sat_att[i],self.sat_location]), lines=np.array([2,0,1]))
+            self.actor['satellite_axis'].append(self.pl.add_mesh(temp, line_width = 1.5, color = self.color[i], name = 'satellite_axis%d' % i))
+
+        for i in range(3):
+            temp = pv.PolyData(np.array([[0,0,0], basis[i]/500*self.earth_radius*1.3]), lines=np.array([2,0,1]))
+            self.actor['earth_axis'].append(self.pl.add_mesh(temp,line_width = 1.5, color = self.color[i], name = 'earth_axis%d' % i))
+
+        self.pl.add_key_event('t',self.trajectory_callback)
+        self.pl.add_key_event('s',self.satellite_callback)
+        self.pl.add_key_event('v',self.satellite_vector_callback)
+        self.pl.add_key_event('e',self.earth_axis_callback)
+
+        self.pl.show(interactive_update=True)
+
+        prev_time = time.time()
+
+        while True:
+            if data_update != None:
+                self.update_satellite()
+
+            if tle_update != None:
+                print('hi')
+                self.update_trajectory()
+                
+            self.pl.update(stime = 1, force_redraw = False)
+            
+            if time.time()- prev_time > 1000:
+                prev_time = time.time()
+                self.pl.update()
+
+    def trajectory_callback(self):
+        print('key pushed t')
+        if not self.trajectory_exist:
+            return
+        
+        if self.trajectory_active:
+            self.lock.acquire()
+            for ele in self.actor['satellite_dark_trajectory']:
+                self.pl.remove_actor(ele)
+            for ele in self.actor['satellite_bright_trajectory']:
+                self.pl.remove_actor(ele)
+            self.lock.release()
+            self.trajectory_active = False
+        else :
+            self.lock.acquire()
+            for ele in self.actor['satellite_dark_trajectory']:
+                self.pl.add_actor(ele)
+            for ele in self.actor['satellite_bright_trajectory']:
+                self.pl.add_actor(ele)
+            self.lock.release()
+            self.trajectory_active = True
+
+    def satellite_vector_callback(self):
+        if self.satellite_vector_active :
+            self.lock.acquire()
+            for ele in self.actor['satellite_vector'].values():
+                self.pl.remove_actor(ele)
+            self.lock.release()
+            self.satellite_vector_active = False
+        else :
+            self.lock.acquire()
+            for ele in self.actor['satellite_vector'].values():
+                self.pl.add_actor(ele)
+            self.lock.release()
+            self.satellite_vector_active = True
+    def satellite_callback(self):
+        if self.satellite_active :
+            for ele in self.actor['satellite_axis']:
+                self.pl.remove_actor(ele)
+            self.satellite_active = False
+        else :
+            for ele in self.actor['satellite_axis']:
+                self.pl.add_actor(ele)
+            self.satellite_active = True
+    def earth_axis_callback(self):
+        if self.earth_axis_active:
+            for ele in self.actor['earth_axis']:
+                self.pl.remove_actor(ele)
+            self.earth_axis_active = False
+        else :
+            for ele in self.actor['earth_axis']:
+                self.pl.add_actor(ele)
+            self.earth_axis_active = True
+
+            
+
+    def update_satellite(self):
+        global data_update
+
+        #print('hi')
+        basis = np.array([[500,0,0],
+                            [0,500,0],
+                            [0,0,500]])
+
+        end_t = data_update.t[:]
+    
+        self.now_time = self.ts.utc(*tuple(data_update.t))
+        self.end_time = self.ts.utc(*tuple(data_update.t[:5] + [data_update.t[5] + 10000]))
+        self.time_gap = self.ts.utc(*tuple(data_update.t[:5] + [range(data_update.t[5],data_update.t[5] + 10000)]))
+        
+        self.sat_location = data_update.location
+        self.sat_vector = data_update.vector
+
+        self.sat_dcm = self.q_t_d(data_update.attitude)
+        sat_att = (self.sat_dcm @ basis).T
+
+        '''
+        for axis in self.actor['satellite_axis']:
+            self.pl.remove_actor(axis)
+        self.actor['satellite_axis'] = []
+        '''
+
+        for i in range(3):
+            self.polydata['satellite_axis%d' % i] = pv.PolyData(np.array([self.sat_location + sat_att[i],self.sat_location]), lines=np.array([2,0,1]))
+            self.actor['satellite_axis'].append(self.pl.add_mesh(self.polydata['satellite_axis%d' % i], line_width = 1.5, color = self.color[i], name = 'satellite_axis%d' % i))
+
+        for vector_name in self.actor['satellite_vector'].keys():
+            self.pl.remove_actor(self.actor['satellite_vector'][vector_name])
+            self.actor['satellite_vector'][vector_name] = None
+
+        for (name, color, vector), i in zip(self.sat_vector, range(len(self.sat_vector))):
+            self.polydata['satellite_vector%d' % i] = pv.PolyData(np.array([self.sat_location , self.sat_location + 400*(self.sat_dcm@((vector).T)).T]), lines=np.array([2,0,1]))
+            self.actor['satellite_vector'][name] = (self.pl.add_mesh(self.polydata['satellite_vector%d' % i], line_width = 1.5, color = color, name = 'satellite_vector%d' % i))
+
+        data_update = None
+    
+    def update_trajectory(self):
+        global tle_update
+        if self.time_gap == None:
+            return
+
+        self.satellite = EarthSatellite(tle_update[1], tle_update[2], tle_update[0], self.ts)
+        print(self.satellite)
+
+        sat_trac = [[],[]]
+        sat_trac_bright = []
+        sat_trac_dark = []
+
+        key = True
+
+        for ti in self.time_gap:
+            loc = self.lat_lon_at(self.satellite, ti, self.earth_radius)
+            sunlit = self.satellite.at(ti).is_sunlit(self.eph)
+        
+            if sunlit :
+                if key == False:
+                    if len(sat_trac_dark) : 
+                        sat_trac_dark.append(loc)
+                        sat_trac[1].append(np.array(sat_trac_dark))
+                        sat_trac_dark = []
+                    
+                    key = True
+                sat_trac_bright.append(loc)
+        
+            else:
+                if key == True:
+                    if len(sat_trac_bright) :
+                        sat_trac_bright.append(loc)
+                        sat_trac[0].append(np.array(sat_trac_bright))
+                        sat_trac_bright = []
+
+                    key = False
+                sat_trac_dark.append(loc)
+
+        if key == True:
+            sat_trac[0].append(np.array(sat_trac_bright))
+        else :
+            sat_trac[1].append(np.array(sat_trac_dark))
+
+        actor_dark, actor_bright = [], []
+
+        for ele in self.actor['satellite_dark_trajectory']:
+            self.pl.remove_actor(ele)
+
+        for ele in self.actor['satellite_bright_trajectory']:
+            self.pl.remove_actor(ele)
+
+        for ele in sat_trac[0]:
+            temp = pv.PolyData(ele, lines=np.array([[2,i,i+1] for i in range(len(ele)-1)]))
+            actor_bright.append(self.pl.add_mesh(temp, line_width = 1.5, color = 'white'))
+
+        for ele in sat_trac[1]:
+            temp = pv.PolyData(ele, lines=np.array([[2,i,i+1] for i in range(len(ele)-1)]))
+            actor_dark.append(self.pl.add_mesh(temp, line_width = 1.5, color = 'gray'))
+
+        self.actor['satellite_dark_trajectory'] = actor_dark
+        self.actor['satellite_bright_trajectory'] = actor_bright
+
+        tle_update = None
+        self.trajectory_exist = True
+
+    def normalize(self, value):
+        if value > 1:
+            return 1
+        elif value < -1:
+            return -1
+        
+        return value
+
+    def q_t_d(self, quaternion):
+        q1, q2, q3,q4 = quaternion
+
+        return np.array([[(q4**2 + q1**2 -q2**2-q3**2), 2*(q1*q2+q3*q4), 2*(q1*q3-q2*q4)],
+                        [2*(q1*q2-q3*q4), (q4**2-q1**2 + q2**2 - q3**2), 2*(q2*q3 + q1*q4)],
+                        [2*(q1*q3+q2*q4), 2*(q2*q3 - q1*q4), (q4**2 - q1**2 - q2**2 + q3**2)]])
+
+    def lat_lon_at(self, satellite, ti, earth_radius):
+        geo = satellite.at(ti)
+
+        lat, lon = wgs84.latlon_of(geo)
+        lat, lon = lat.arcminutes()/60, lon.arcminutes()/60
+        height = wgs84.height_of(geo).km
+
+        return self.lat_lon_rotation(lat, lon, np.array([earth_radius + height, 0, 0]).T)
+
+    def lat_lon_rotation(self, lat, lon, vector):
+        s = math.sin(lat * math.pi / 180)
+        c = math.cos(lat * math.pi / 180)
+        dcm_y = np.array([[c, 0, s],
+                        [0, 1, 0],
+                        [-s, 0, c]])
+
+        s = math.sin(lon * math.pi / 180)
+        c = math.cos(lon * math.pi / 180)
+        dcm_z = np.array([[c, -s, 0],
+                        [s, c, 0],
+                        [0, 0, 1]])
+
+        res = (dcm_z @ (dcm_y @ vector))
+
+        return res.T
+
+def rotation_matrix_from_vectors(vec1, vec2):
+    a, b = (vec1 / np.linalg.norm(vec1)).reshape(3), (vec2 / np.linalg.norm(vec2)).reshape(3)
+    v = np.cross(a, b)
+    c = np.dot(a, b)
+    s = np.linalg.norm(v)
+    kmat = np.array([[0, -v[2], v[1]], [v[2], 0, -v[0]], [-v[1], v[0], 0]])
+    rotation_matrix = np.eye(3) + kmat + kmat.dot(kmat) * ((1 - c) / (s ** 2))
+    return rotation_matrix
+
+def data_receiving(client_socket,adress):
+    #buf = [0 for i in range(1000)]
+    
+    while True:
+        try: 
+            buf = client_socket.recv(1024)
+        except:
+            client_socket.close()
+            return 
+
+        if buf == b'':
+            client_socket.close()
+            return
+
+        data_processing(buf)
+
+
+#0, 1 시작 개시 2번 통신 코드
+#0번은 time
+#1번은 location
+#2번은 attitude
+#3번은 vector
+#4번은 이를 적용
+#0번은 
+
+def data_processing(buf):
+    global recieve_time, recieve_location, recieve_attitude, recieve_vec, recieve_tle
+    global data_queue, data_update, tle_update
+    
+
+    while len(buf) > 1:
+        #print(buf)
+        if buf[0] != 0x96 or buf[1] !=0x00:
+            return
+
+        if buf[2] == 0x00:
+            _, _, _, year, month, day, hour, min, sec = struct.unpack('3Bi5B', buf[:13])
+            recieve_time = [year, month, day, hour, min, sec]
+            #print(len(buf),buf,recieve_time)
+            buf = buf[13:]
+        elif buf[2] == 0x01:
+            _, _, _, x, y, z = struct.unpack('3B3f',buf[:16])
+            recieve_location = np.array([x, y ,z])
+            #print(len(buf),buf, recieve_location)
+            buf = buf[16:]
+        elif buf[2] == 0x02:
+            _, _, _, q1, q2, q3, q4 = struct.unpack('3B4f',buf[:20])
+            recieve_attitude = np.array([q1, q2, q3, q4])
+            #print(len(buf),buf, recieve_attitude)
+            buf = buf[20:]
+        elif buf[2] == 0x03:
+            print(buf,len(buf[:18 + buf[3] + buf[4]]))
+            _, _, _, _, _,  x, y, z, name, color = struct.unpack(('5B3f%ds%ds' % (buf[3],buf[4])),buf[:20 + buf[3] + buf[4]])
+            
+            recieve_vec.append((name.decode(),color.decode(),np.array([x, y ,z])))
+            print(len(buf),buf, recieve_vec)
+            buf = buf[20 + buf[3] + buf[4]:]
+        elif buf[2] == 0x04:
+            data_queue.append(satrac_info(recieve_time, recieve_location, recieve_attitude, recieve_vec))
+            data_update = data_queue[-1]
+            #print(data_queue, data_update)
+            
+            recieve_time = None
+            recieve_location = None
+            recieve_attitude = None
+            recieve_vec = []
+            buf = buf[3:]
+
+        elif buf[2] == 0x05:
+            _, _, _, _, _, _, line1, line2, line3 = struct.unpack(('3B3B%ds%ds%ds' % (buf[3],buf[4],buf[5])),buf[:6+buf[3]+buf[4]+buf[5]])
+            tle_update = [line1.decode(), line2.decode(), line3.decode()]
+            buf = buf[6+buf[3]+buf[4]+buf[5]:]
+        
+        else :
+            buf = []
+
+def take_client_connection():
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server_socket.bind(('127.0.0.1', 30000))
+    server_socket.listen(5)
+
+
+    while True:
+        (client_socket, address) = server_socket.accept()
+
+        th1 = Thread(target=data_receiving, args=(client_socket, address), daemon = True)
+        th1.start()
+
+
+recieve_time = None
+recieve_location = None
+recieve_attitude = None
+recieve_vec = []
+recieve_tle = None
+
+tle_update = None
+
+data_update = None
+data_queue = []
+
+gs_location = [6378.1,0,0]
+
+#reader = pv.get_reader('./data/satellite.stl')
+#sat_mesh = reader.read()
+#sat_scale = 30
+
+th1 = Thread(target=take_client_connection, daemon= True)
+th1.daemon = True
+th1.start()
+
+
+new_program = program_manage(gs_location, np.array([6378.1*1.3, 0, 0]))
+new_program.start_program()
---
+++ testclient.py
@@ -0,0 +1,115 @@
+import socket
+import struct
+import time
+import numpy
+import math
+import numpy as np
+from skyfield.api import load, wgs84
+
+def time_packet(year, month, day, hour, min, sec):
+
+    return struct.pack('3Bi5B',0x96, 0x00, 0x00,year, month, day, hour, min, sec)
+
+def attitude_packet(quaternion):
+    q1, q2, q3, q4 = quaternion
+
+    return struct.pack('3B4f',0x96,0x00,0x02, q1, q2, q3, q4)
+
+def location_packet(location):
+    x, y, z = location
+    return struct.pack('3B3f',0x96,0x00,0x01,x,y,z)
+
+def vector_packet(name, color, vector):
+    x, y, z = vector
+    name, color = name.encode(), color.encode()
+
+    return struct.pack(('5B3f%ds%ds' % (len(name), len(color))),0x96,0x00,0x03,len(name),len(color),x,y,z,name, color)
+
+def tle_packet(line1, line2, line3):
+    line1, line2, line3 = line1.encode(), line2.encode(), line3.encode()
+    
+    return struct.pack(('3B3B%ds%ds%ds' % (len(line1),len(line2), len(line3))),0x96,0x00,0x05,len(line1),len(line2),len(line3),line1,line2,line3)
+
+def tle_end_packet():
+    return struct.pack('3B',0x96,0x00,0x06)
+
+def satellite_end_packet():
+    return struct.pack('3B',0x96, 0x00, 0x04)
+
+def lat_lon_at(satellite, ti, earth_radius):
+    geo = satellite.at(ti)
+
+    lat, lon = wgs84.latlon_of(geo)
+    lat, lon = lat.arcminutes()/60, lon.arcminutes()/60
+    height = wgs84.height_of(geo).km
+
+    return lat_lon_rotation(lat, lon, np.array([earth_radius + height, 0, 0]).T)
+
+def lat_lon_rotation(lat, lon, vector):
+    s = math.sin(lat * math.pi / 180)
+    c = math.cos(lat * math.pi / 180)
+    dcm_y = np.array([[c, 0, s],
+                      [0, 1, 0],
+                      [-s, 0, c]])
+
+    s = math.sin(lon * math.pi / 180)
+    c = math.cos(lon * math.pi / 180)
+    dcm_z = np.array([[c, -s, 0],
+                      [s, c, 0],
+                      [0, 0, 1]])
+
+    res = (dcm_z @ (dcm_y @ vector))
+
+    return res.T
+
+client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client_socket.connect(('127.0.0.1', 30000))
+
+earth_radius = 6378.1
+loca = [np.array([earth_radius*1.3, 0, 0]), np.array([0, 8000, 0])]
+quaternion = [0,(1/2)**(1/2),0,(1/2)**(1/2)]
+
+stations_url = 'https://celestrak.org/NORAD/elements/gp.php?INTDES=2022-072'
+satellites = load.tle_file(stations_url, reload=True)
+print('Loaded', len(satellites), 'satellites')
+
+satellite = {sat.model.satnum: sat for sat in satellites}[52935]
+print(satellite)
+
+line1 = 'DS-EO'
+line2 = '1 52935U 22072A   22216.15420356 -.00001113  00000+0 -14437-3 0  9995'
+line3 = '2 52935   9.9958 183.1620 000691  87.5122 272.5811 14.99607335  5188'
+
+ts = load.timescale()
+t = ts.now()
+i=0
+time_gap = ts.utc(2022,8,5,0,0,range(0,120))
+packet_list = []
+client_socket.send(time_packet(2022,8,5,0,0,0))
+client_socket.send(attitude_packet([0,0,0,1]))
+client_socket.send(location_packet([1,0,0]))
+client_socket.send(satellite_end_packet())
+#client_socket.send(tle_packet(line1, line2, line3))
+
+#time.sleep(10)
+
+for ti in time_gap:
+    loc = lat_lon_at(satellite, ti, earth_radius)
+    a = np.random.randint(10, size=4) 
+    a = a / np.linalg.norm(a)
+    p1 = time_packet(2022,8,5,0,0,0)
+    p2 = attitude_packet(a)
+    p3 = location_packet(loc)
+    p4 = vector_packet('helasdlo','purple',[1,1,1])
+    p5 = satellite_end_packet()
+
+    packet_list.append((p1, p2, p3, p4, p5))
+
+print([packet_list])
+
+for ele in packet_list:
+    for pac in ele:
+        client_socket.send(pac)
+    time.sleep(1)
+
+client_socket.close()
